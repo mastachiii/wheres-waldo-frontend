715b5eed5850d6947db8b3b7bc30d0704aeaef48
mastachii <mastachii273@gmail.com>
2025-02-02 17:16:26 +0800

    feat: Styling for menu + Loading component

---
+++ src/components/loading.jsx
@@ -0,0 +1,5 @@
+import loading from "../assets/loading.svg";
+
+export default function Loading() {
+    return <img src={loading} className="animate-spin w-[5rem] h-[90vh] m-auto " />;
+}
--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -3,6 +3,7 @@ import levelApi from "../helpers/levelApi";
 import { Link } from "react-router";
 import { charImages, sceneImages } from "../helpers/levelImages";
 import { showLevelCharacters } from "../helpers/transformLevelData";
+import Loading from "./loading";
 
 export default function Menu() {
     const [levels, setLevels] = useState([]);
@@ -12,38 +13,48 @@ export default function Menu() {
         (async () => {
             const { levels } = await levelApi.getAllLevels();
 
-            setLevels(levels);
+            setTimeout(() => {
+                setLevels(levels);
+            }, 1000);
         })();
     }, []);
 
-    return (
-        <div>
-            <h4 className=" font-mono text-3xl font-semibold text-center mt-10 hover:">Levels</h4>
-            <ol className="mh-[85vh] flex w-[70%]  gap-20 justify-center ml-auto mr-auto p-10 ">
-                {levels.map(l => {
-                    const charsToShow = showLevelCharacters({ levelData: l });
-                    return (
-                        <li
-                            key={l.id}
-                            onMouseEnter={() => setSelected(l.id)}
-                            onMouseLeave={() => setSelected(null)}
-                            className={`w-[25%] h-[40%] p-5 pb-10 rounded-lg text-[10px] shadow-lg  bg-stone-100 transition duration-100 ease-in hover:bg-amber-300 hover:scale-110 *: hover:opacity-100 ${
-                                selected === l.id ? "h-[50%]" : null
-                            }`}
-                        >
-                            <img src={sceneImages[l.name]} alt="level image" className="mb-2" />
-                            <Link to={`/levels/${l.id}`} className=" font-mono italic hover:text-blue-700">
-                                {l.sceneName}
-                            </Link>
-                            <div className={selected === l.id ? "block" : "hidden"}>
-                                {charsToShow.map(c => {
-                                    return <p>{c}</p>;
-                                })}
-                            </div>
-                        </li>
-                    );
-                })}
-            </ol>
-        </div>
-    );
+    if (levels.length === 0) {
+        return <Loading />;
+    } else {
+        return (
+            <div>
+                <h4 className=" font-mono text-3xl font-semibold text-center mt-10 hover:">Levels</h4>
+                <ol className="mh-[85vh] flex w-[70%]  gap-20 justify-center ml-auto mr-auto p-10 ">
+                    {levels.map(l => {
+                        const charsToShow = showLevelCharacters({ levelData: l });
+                        return (
+                            <li
+                                key={l.id}
+                                onMouseEnter={() => setSelected(l.id)}
+                                onMouseLeave={() => setSelected(null)}
+                                className={`w-[25%] h-[40%] p-5 pb-10 rounded-lg text-[10px] shadow-lg  bg-stone-100
+                                 transition duration-100 ease-in hover:bg-amber-300 hover:scale-110 *: hover:opacity-100 ${
+                                     selected === l.id && "h-[50%] border-2  border-stone-600"
+                                 }`}
+                            >
+                                <img src={sceneImages[l.name]} alt="level image" className="mb-2" />
+                                <Link to={`/levels/${l.id}`} className={`font-mono italic hover:text-blue-700 ${selected === l.id && "text-[15px]"}`}>
+                                    {l.sceneName}
+                                </Link>
+                                <div className={selected === l.id ? "block font-mono mt-2 " : "hidden"}>
+                                    <p className="mb-2">Characters to find:</p>
+                                    <div className="flex gap-2 flex-wrap">
+                                        {charsToShow.map(c => {
+                                            return <img src={charImages[c]} key={c} className="w-[15%] rounded-full" />;
+                                        })}
+                                    </div>
+                                </div>
+                            </li>
+                        );
+                    })}
+                </ol>
+            </div>
+        );
+    }
 }
