fc303fc41eca2ea79e34ced595f21508cbf4400e
mastachii <mastachii273@gmail.com>
2025-02-02 21:44:33 +0800

    feat: Helper for converting time into minutes : seconds

---
+++ src/helpers/convertSeconds.js
@@ -0,0 +1,10 @@
+// Convert seconds into a more hunan-friendly text
+
+export default function convertSeconds(sec) {
+    let minutes = String(Math.floor(sec / 60));
+    let seconds = String(sec - minutes * 60);
+
+    if (seconds.length === 1) seconds = "0" + seconds;
+
+    console.log(`${minutes}:${seconds}`);
+}
