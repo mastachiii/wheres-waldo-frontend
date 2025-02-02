7554b16c0b1e5bf06cfbd1f925634fe164277d3c
mastachii <mastachii273@gmail.com>
2025-02-02 19:46:46 +0800

    wip: Styling character selection

--- src/components/characterSel.jsx
+++ src/components/characterSel.jsx
@@ -1,5 +1,6 @@
 import PropTypes, { bool, number } from "prop-types";
 import { showLevelCharacters } from "../helpers/transformLevelData";
+import { charImages } from "../helpers/levelImages";
 
 export default function CharacterSelection({ x, y, active, activeHandler, charHandler, levelData }) {
     const charsToShow = showLevelCharacters({ levelData, mapToEmptyStrings: false });
@@ -13,9 +14,20 @@ export default function CharacterSelection({ x, y, active, activeHandler, charHa
     }
 
     return (
-        <div style={{ display: active ? "block" : "none", position: "absolute", top: `${y}px`, left: `${x}px` }} onClick={handleClick}>
+        <div
+            style={{ display: active ? "flex" : "none", position: "absolute", top: `${y}px`, left: `${x - 20}px` }}
+            onClick={handleClick}
+            className=" flex-col justify-center p-3 overflow-hidden"
+        >
             {charsToShow.map(c => {
-                return <button key={c}>{c}</button>;
+                return (
+                    <button key={c}>
+                        <img
+                            src={charImages[c]}
+                            className="w-[3rem] h-[3rem] rounded-full transition duration-75 ease-in cursor-pointer hover:scale-120"
+                        />
+                    </button>
+                );
             })}
         </div>
     );
