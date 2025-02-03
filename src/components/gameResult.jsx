1b4be44de20f22b385aa6b141ea19f7c5567f81c
mastachii <mastachii273@gmail.com>
2025-02-03 08:58:27 +0800

    feat: Styling for result page

--- src/components/gameResult.jsx
+++ src/components/gameResult.jsx
@@ -3,21 +3,23 @@ import convertSeconds from "../helpers/convertSeconds";
 
 export default function Result({ timeFinished, attempts }) {
     return (
-        <div className="w-[30%] flex flex-col justify-center text-center ml-auto mr-auto translate-y-50 font-mono">
+        <div className="w-[30%] flex flex-col justify-center text-center ml-auto mr-auto translate-y-50 font-mono animate-popUp">
             <h4 className="text-5xl mb-7">You Won!</h4>
             <p>Your time: {convertSeconds(timeFinished)}</p>
-            <ul className="flex flex-col mt-5">
-                <p className="text-sm text-start">Leaderboards: </p>
+            <p className="text-sm text-start mt-10">Leaderboards: </p>
+            <ul className="flex">
                 {attempts.map((a, index) => {
                     return (
-                        <span key={a.id} className="flex align-middle mt-3 ml-5">
+                        <span key={a.id} className="flex mt-3 ml-5">
                             <p className="text-sm mt-1 mr-2">{index + 1}.</p>
                             <p className="">{` ${a.playerName} - ${convertSeconds(a.timeFinished)}`}</p>
                         </span>
                     );
                 })}
             </ul>
-            <Link to={"/levels"} className="mt-300">Back to menu</Link>
+            <Link to={"/levels"} className="mt-30 underline hover:text-amber-300">
+                Back to menu
+            </Link>
         </div>
     );
 }
